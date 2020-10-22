ae751b5018c92bbfca3a82485ac630cd0fdd1eff
SusannaMontero <49920436+SusannaMontero@users.noreply.github.com>
2020-10-22 20:58:37 +0200

    Add files via upload

---
+++ DEURES_DAW2/ColdWar/src/Partida.java
@@ -0,0 +1,30 @@
+
+public class Partida {
+	
+	//atributos
+	
+		 String ganadorPartida;
+		 //5 objetos de la clase Planeta;
+		 
+		//m�todos
+		 
+		 //constructor;
+		 
+		 public void iniciarPartida {
+			 
+		 }
+
+		 public int comprobarequiposvivos {
+			 
+		 }
+		 
+		 public void mostrarGanador {
+			 
+		 }
+		 
+		 public void finalizarPartida {
+			 
+		 }
+	}
+
+
---
+++ DEURES_DAW2/ColdWar/src/Planeta.java
@@ -0,0 +1,31 @@
+
+public class Planeta {
+	
+	//atributos
+	
+		private int vidas;
+		private String nombre;
+		private int misiles_ronda;
+		
+		
+		
+		//M�todos
+		
+		//constructor
+		public void iniciarPartida() {
+			
+		}
+
+		public int comprobarequiposvivos() {
+			
+		}
+		
+		public void mostrarGanador()	{
+			
+		}
+		
+		public void finalizarPartida()	{
+			
+		}
+	}
+
